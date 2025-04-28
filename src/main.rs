2f4caebf5fdfdcffe65eb2a6b37b63fb47e1d2ee
John Agapeyev <jagapeyev@gmail.com>
2025-04-27 22:57:05 -0700

    Tweak init of uring setup call

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use liburing_sys::*;
 
 fn main() {
-    unsafe { io_uring_setup(32, std::ptr::null_mut()) };
+    unsafe {
+        let mut params: io_uring_params = std::mem::zeroed();
+        io_uring_setup(32, &mut params);
+    };
     println!("Hello, world!");
 }
