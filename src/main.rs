cc24bcd7d80d44b6489b83b097aa21e3a8f4d7f9
John Agapeyev <jagapeyev@gmail.com>
2025-04-29 14:14:33 -0700

    Server listening is now working

--- src/main.rs
+++ src/main.rs
@@ -48,20 +48,11 @@ struct Cli {
     port: u16,
 }
 
-fn main() {
-    let cli = Cli::parse();
-
-    if cli.listen {
-        println!("We don't currently support server listening mode!");
-        return;
-    }
-
+fn client(host: String, port: u16) {
     unsafe {
         let mut ring = Ring::new(32, 0).unwrap();
 
         let mut magic: i32 = 17;
-
-        let now = Instant::now();
 
         let connect_sqe = get_sqe(&mut ring.inner).unwrap();
 
@@ -75,7 +66,7 @@ fn main() {
         )
         .unwrap();
 
-        let addr = SockaddrIn::from_str(&format!("{}:{}", cli.host, cli.port)).unwrap();
+        let addr = SockaddrIn::from_str(&format!("{}:{}", host, port)).unwrap();
 
         io_uring_prep_connect(
             connect_sqe,
@@ -84,36 +75,172 @@ fn main() {
             addr.len(),
         );
 
-        let send_sqe = get_sqe(&mut ring.inner).unwrap();
-
-        io_uring_sqe_set_data(send_sqe, &mut magic as *mut _ as *mut c_void);
-
-        let msg = "Hello io_uring world!\n";
-
-        io_uring_prep_send(
-            send_sqe,
-            sock.as_raw_fd(),
-            msg.as_bytes().as_ptr() as *const c_void,
-            msg.len(),
-            0,
-        );
-
-        let submitted = io_uring_submit(&mut ring.inner);
-        println!("Submitted {submitted} entries");
+        let _ = io_uring_submit(&mut ring.inner);
 
         let mut cqe: *mut io_uring_cqe = std::ptr::null_mut();
 
         let res = io_uring_wait_cqe(&mut ring.inner, &mut cqe);
-        println!("Wait CQE result was: {res}");
-
         assert!(res == 0);
 
-        let raw = io_uring_cqe_get_data(cqe);
-        assert!(!raw.is_null());
+        let mut count = 0usize;
 
-        println!("Time taken was: {}", now.elapsed().as_micros());
+        loop {
+            let now = Instant::now();
+            count += 1;
 
-        println!("My magic value is: {}", *(raw as *mut i32));
+            let send_sqe = get_sqe(&mut ring.inner).unwrap();
+
+            io_uring_sqe_set_data(send_sqe, &mut magic as *mut _ as *mut c_void);
+
+            let msg = "Hello io_uring world!\n";
+
+            io_uring_prep_send(
+                send_sqe,
+                sock.as_raw_fd(),
+                msg.as_bytes().as_ptr() as *const c_void,
+                msg.len(),
+                0,
+            );
+
+            let _ = io_uring_submit(&mut ring.inner);
+
+            let mut cqe: *mut io_uring_cqe = std::ptr::null_mut();
+
+            let res = io_uring_wait_cqe(&mut ring.inner, &mut cqe);
+            assert!(res == 0);
+
+            let raw = io_uring_cqe_get_data(cqe);
+            assert!(!raw.is_null());
+
+            println!(
+                "Time taken was: {} for message {}",
+                now.elapsed().as_nanos(),
+                count
+            );
+        }
     };
-    println!("Hello, world!");
+}
+
+fn server(host: String, port: u16) {
+    unsafe {
+        let mut ring = Ring::new(32, 0).unwrap();
+
+        let mut magic: i32 = 17;
+
+        let bind_sqe = get_sqe(&mut ring.inner).unwrap();
+        io_uring_sqe_set_data(bind_sqe, &mut magic as *mut _ as *mut c_void);
+
+        let sock = socket(
+            AddressFamily::Inet,
+            SockType::Stream,
+            SockFlag::SOCK_NONBLOCK,
+            SockProtocol::Tcp,
+        )
+        .unwrap();
+
+        let addr = SockaddrIn::from_str(&format!("{}:{}", host, port)).unwrap();
+
+        io_uring_prep_bind(
+            bind_sqe,
+            sock.as_raw_fd(),
+            addr.as_ptr() as *mut liburing_sys::sockaddr,
+            addr.len(),
+        );
+
+        let _ = io_uring_submit(&mut ring.inner);
+
+        let mut cqe: *mut io_uring_cqe = std::ptr::null_mut();
+
+        let res = io_uring_wait_cqe(&mut ring.inner, &mut cqe);
+        assert!(res == 0);
+
+        io_uring_cqe_seen(&mut ring.inner, cqe);
+
+        let listen_sqe = get_sqe(&mut ring.inner).unwrap();
+        io_uring_sqe_set_data(listen_sqe, &mut magic as *mut _ as *mut c_void);
+
+        io_uring_prep_listen(listen_sqe, sock.as_raw_fd(), 32);
+
+        let _ = io_uring_submit(&mut ring.inner);
+
+        let mut cqe: *mut io_uring_cqe = std::ptr::null_mut();
+
+        let res = io_uring_wait_cqe(&mut ring.inner, &mut cqe);
+        assert!(res == 0);
+
+        io_uring_cqe_seen(&mut ring.inner, cqe);
+
+        let accept_sqe = get_sqe(&mut ring.inner).unwrap();
+        io_uring_sqe_set_data(accept_sqe, &mut magic as *mut _ as *mut c_void);
+
+        io_uring_prep_accept(
+            accept_sqe,
+            sock.as_raw_fd(),
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+            SockFlag::SOCK_NONBLOCK.bits(),
+        );
+
+        let _ = io_uring_submit(&mut ring.inner);
+
+        let mut cqe: *mut io_uring_cqe = std::ptr::null_mut();
+
+        let res = io_uring_wait_cqe(&mut ring.inner, &mut cqe);
+        assert!(res == 0);
+        assert!(!cqe.is_null());
+        assert!((*cqe).res > 0);
+
+        println!("Resulting socket fd is: {}", (*cqe).res);
+
+        let accepted_sock = (*cqe).res;
+
+        io_uring_cqe_seen(&mut ring.inner, cqe);
+
+        let mut count = 0usize;
+
+        loop {
+            let now = Instant::now();
+            count += 1;
+
+            let send_sqe = get_sqe(&mut ring.inner).unwrap();
+
+            io_uring_sqe_set_data(send_sqe, &mut magic as *mut _ as *mut c_void);
+
+            let msg = "Hello io_uring world!\n";
+
+            io_uring_prep_send(
+                send_sqe,
+                accepted_sock,
+                msg.as_bytes().as_ptr() as *const c_void,
+                msg.len(),
+                0,
+            );
+
+            let _ = io_uring_submit(&mut ring.inner);
+
+            let mut cqe: *mut io_uring_cqe = std::ptr::null_mut();
+
+            let res = io_uring_wait_cqe(&mut ring.inner, &mut cqe);
+            assert!(res == 0);
+
+            let raw = io_uring_cqe_get_data(cqe);
+            assert!(!raw.is_null());
+
+            println!(
+                "Time taken was: {} for message {}",
+                now.elapsed().as_nanos(),
+                count
+            );
+        }
+    };
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    if cli.listen {
+        server(cli.host, cli.port);
+    } else {
+        client(cli.host, cli.port);
+    }
 }
