f94622333c080dc1458e8bcdc55ba8a5751d2990
John Agapeyev <jagapeyev@gmail.com>
2025-05-24 14:07:54 -0700

    Write up my reasoning for deviating from the AsyncWrite trait compatibility approach

--- src/tcp.rs
+++ src/tcp.rs
@@ -12,6 +12,56 @@ use futures::io::AsyncWrite;
 use nix::sys::socket::{AddressFamily, SockProtocol, SockType, SockaddrIn, SockaddrLike};
 use pin_project::pin_project;
 
+
+/*
+ * I don't like the current situation with handling state around the AsyncRead/AsyncWrite trait
+ * implementation requirements with polling
+ * Fundamentally, io_uring is an ownership based system because it focuses on completions
+ * The tactics used by futures and tokio don't work here because of the ownership need.
+ * So when you try to write a buffer, tokio will poll for readiness and then do the call directly.
+ * Doing AsyncWriteExt calls on a type returns a trivial wrapper that implements Future simply
+ * by delegating the poll to the poll_write of the underlying AsyncWrite type
+ *
+ * This becomes quite a struggle around spurious wakeup polling, since in a stateless reference
+ * world like futures/tokio, you can simply redo the same operations with no change.
+ * However, on the io_uring side, you've already submitted the operation and need to wait for
+ * completion.
+ *
+ * So how do you "know" what operation to poll in the delegation to?
+ * You need some state object to reference to poll
+ * But you can't store that state inside the object/future itself due to reference sharing race
+ * conditions.
+ * Say for example you are implenting a UDP socket.
+ * It's totally valid to use a Rc<OwnedFd> and share it between async tasks.
+ * The kernel syscalls are atomic and well-defined, and the SQE fetching is as well.
+ * The problem is on the state handling.
+ * If you store the state inside the UDP socket type, what happens on a spurious wakeup?
+ * You now have two valid instances of write that requires state that needs polling.
+ * Which obviously generalizes up to N, requiring dynamic size lists here.
+ * But even if you make a Vec<Future> for yourself, now you need to know which one to poll!
+ *
+ * What do you have to identify the specific operation?
+ * In the case of write, it's the buffer slice, but now you're doing a full memcmp just to re-poll?
+ * The entire mess gets very silly very quickly.
+ *
+ * I'm already going to have to go out into the unknown when trying to do fancier things like
+ * linking SQEs together and juggling registered files/buffers.
+ * So perhaps the pain of trying to stay compatible with legacy community traits is holding me back
+ * here?
+ * I need ownership, make a trait with ownership.
+ * The whole reason these wrapper types and AsyncWrite's poll_write() function exist in the first
+ * place is due to Rust language limitations with async and traits!
+ * We now have async traits since 1.75, with some big limitations, but still, there's no need to
+ * juggle all of this chaos for compatibility that is going to be knowingly suboptimal in the first
+ * place!
+ *
+ * Better to do your own thing and borrow naming and conventions rather than trying to explicitly
+ * remain compatible with that part of the ecosystem.
+ * The whole reason I'm building this project is to avoid the traditional io_uring integrations
+ * that are done by libuv/ASIO and others that still end up wrapping epoll under the hood.
+ * It's not just a different faster poll(), it's closer to IOCP, and should be treated as such.
+ */
+
 #[pin_project]
 pub struct TcpStream {
     sock: OwnedFd,
